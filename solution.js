b9fba3b1924f5fa9613c13b67aaa5bfae7c9f970
BorisDmitriev <113904873+BorisDmitriev@users.noreply.github.com>
2023-10-09 21:29:22 +0200

    Initial commit

---
+++ solution.js
@@ -0,0 +1,7 @@
+const myNum = 15;
+
+console.log( myNum % 3 === 0 ? "Fizz" : myNum );
+console.log( myNum % 5 === 0 ? "Buzz" : myNum );
+console.log( myNum % 3 === 0 && myNum % 5 === 0? "FizzBuzz" : myNum );
+
+
